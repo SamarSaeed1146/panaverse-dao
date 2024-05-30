b787f9954142fbf2f0cb205a4795f3f32bc9ece7
SamarSaeed1146 <samarsaeed374@gmail.com>
2024-05-30 14:15:26 +0500

    panaverse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="font-bold bg-red-500">hello world</div>
-      <div className="font-bold bg-yellow-200">Muhammad Samar Saeed</div>
     </main>
   );
 }
